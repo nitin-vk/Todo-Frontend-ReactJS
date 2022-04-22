4674446cc4a2f7ca59f543bba49035d6d391bef3
nitin-vk <nitinvkavya@gmail.com>
2022-04-22 21:07:01 +0530

    changes made to the endpoint url

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -10,7 +10,7 @@ function DeleteModal(props) {
         alert('Delete failed');
         props.closeModal();
       } else {
-        props.getItems();
+        return response.json();
       }
     });
   }
